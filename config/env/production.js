b2fec0dc947b7e45199cde0a82b46fa706177d8b
whatdadeuce <whatdadeuce@users.noreply.github.com>
2018-04-18 22:33:03 -0400

    -Revise production.js

--- config/env/production.js
+++ config/env/production.js
@@ -1,4 +1,3 @@
 module.exports = {
-    sessionSecret: 'developmentSessionSecret',
-    URI: "mongodb://admin:comp308-2018@ds239439.mlab.com:39439/comp308-project"
+    sessionSecret: 'developmentSessionSecret'
 };
