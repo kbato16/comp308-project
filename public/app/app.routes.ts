17f692a523ac021cc2c90363e96561aa0415c0ec
Philip Laxamana <philip.laxamana@hotmail.com>
2018-04-21 19:39:06 -0400

    added to signup, profile and navbar

--- public/app/app.routes.ts
+++ public/app/app.routes.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { AppComponent } from './app.component';
 
 const AppRoutes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
+    { path: 'profile', component: ProfileComponent },
     { path: '', component: HomeComponent }
 ];
 
