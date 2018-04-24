90951bfcd879acff8e8d70233f80636c0ed55228
whatdadeuce <kenneth_bato16@yahoo.com>
2018-04-24 11:39:25 -0400

    -Should improve accuracy by initializing vitals properties -Some styling changes

--- public/app/components/vitals/diagnose/diagnose.component.ts
+++ public/app/components/vitals/diagnose/diagnose.component.ts
@@ -21,14 +21,40 @@ export class DiagnoseComponent implements OnInit {
   ngOnInit() {
     this.paramsObserver = this.route.parent.params.subscribe(params => {
       this.patientId = params['patientId'];
+      this.vitals.BodyHeight = '';
+      this.vitals.BodyWeight = '';
+      this.vitals.BodyMassIndex = '';
+      this.vitals.SystolicBloodPressure = '';
+      this.vitals.DiastolicBloodPressure = '';
+      this.vitals.OralTemperature = '';
+      this.vitals.QualityAdjustedLifeYear = '';
+      this.vitals.DisabilityRatingScale = '';
+      this.vitals.NonSmallCellLungCancer = '';
+      this.vitals.HemoglobinA1c = '';
+      this.vitals.Glucose = '';
+      this.vitals.UreaNitrogen = '';
+      this.vitals.Creatinine = '';
+      this.vitals.Calcium = '';
+      this.vitals.Sodium = '';
+      this.vitals.Potassium = '';
+      this.vitals.Chloride = '';
+      this.vitals.CarbonDioxide = '';
+      this.vitals.TotalCholesterol = '';
+      this.vitals.Triglycerides = '';
+      this.vitals.LowDensityLipoproteinCholesterol = '';
+      this.vitals.HighDensityLipoproteinCholesterol = '';
+      this.vitals.MicroalbuminCreatinineRatio = '';
+      this.vitals.EstimatedGlomerularFiltrationRate = '';
+      this.vitals.DXABonedensity = '';
     });
   }
-
+  resetDiagnosis() {
+    this.diagnosis = null;
+    this.router.navigate([`/vitals/${this.patientId}/diagnose`]);
+  }
   diagnose() {
     this.vitalService.diagnose(this.vitals).subscribe(result => {
-      console.log('From diagnose Component' + this.vitals);
       if (result) {
-        console.log('Result: ' +  Object.keys(result));
         this.diagnosis = String(Object.keys(result));
         this.router.navigate([`/vitals/${this.patientId}/diagnose`]);
       }
